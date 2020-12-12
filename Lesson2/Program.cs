df3cba9115470f6ba9d23cf3d23469a58069ca45
Mark <markdavies550@gmail.com>
2020-12-12 13:13:23 +0000

    Doing things again

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,5 +1,5 @@
-﻿// DataType Label;
-// DataType Label = Value;
+﻿// TYPE Label;
+// TYPE Label = Value;
 // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 // Boolean = true|false
@@ -12,37 +12,56 @@ char charType = 'a';
 // int = integer = full number eg. -10, 10, 5, 6 = Anything that hasn't got a decimal place
 int intType = 10;
 int intTypew = -11;
+// -2,147,483,648 , +2,147,483,647
 
 // float = floating point = 11.5
-float floatType = 11.5;
+float floatType = 11.5f;
+// -3.402823e38 ,  +3.402823e38
 
 // decimal = decimal places
-decimal decimalType = 11.5;
+decimal decimalType = 11.5m;
+//+79,228,162,514,264,337,593,543,950,335 , -79,228,162,514,264,337,593,543,950,335
+// The Decimal value type is appropriate for financial calculations that require large numbers of significant integral and fractional digits and no round-off errors
 
 // long = larger version of the int eg. -10, 10, 5, 6
 long longType = 11;
+// - 9,223,372,036,854,775,808 , +9,223,372,036,854,775,807
 
 // short = smaller version of int eg. -10, 10, 5, 6
 short shortType = 11;
+// -32768 , +32767
 
 // uint = unsigned integer = integer - can store negative number, uint - only stores positive numbers eg. 10, 5, 6
 uint uintType = 11;
+// 0 , 4,294,967,295
 
 // ulong = unsigned long = long - can store negative number, ulong - only stores positive numbers eg. 10, 5, 6
 ulong longType = 11;
+// 0, 18,446,744,073,709,551,615
 
 // ushort = unsigned short = short - can store negative number, ushort - only stores positive numbers eg. 10, 5, 6
 ushort shortType = 11;
+// 0, 65535
 
 // byte 
-byte byteType = 0x00;
-sbyte byteType2 = 0x00;
+byte byteType = 64;
+// 0, 255
+
+sbyte byteType2 = 255;
+//-128, 127
 
 // string = Sequence of characters
 // Future stuff :
-int[] intArray = new[]{0,1,2,3,4};
+int[] intArray = new[]{0,4,2,3,4};
 char[] charArray = new[] {'a', 'b', 'c'};
-string message = "";
+//Value ^ 
+
+// Reference type \/
+string message = "hello";
+char character = message[0]; // h
+
+var hello = 1;
+
 
 
 
---
+++ Lesson5/Program.cs
@@ -0,0 +1,32 @@
+﻿var intType = 3;
+var stringType = "Message";
+
+// && = AND 
+// || = OR 
+
+// 1+1*4 = 8
+// 1+(1*4) = 5
+
+if(intType == 4 || stringType == "Message1")
+{
+    System.Console.Write("true");
+}
+if(intType == 3 && (stringType == "Message1" || stringType == "Message" ))
+{
+    System.Console.Write("true 1");
+}
+else 
+{
+    System.Console.Write("false");
+}
+
+// ^ = Exclusive OR
+
+if(stringType == "Message1" ^ intType == 3 )
+{
+    System.Console.Write("XOR true");
+}
+else
+{
+    System.Console.Write("XOR false");
+}
